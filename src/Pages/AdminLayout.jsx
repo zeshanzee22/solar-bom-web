a0dc0b248ad8679eab74af77ad916889c811d538
Quratulain983 <quratulain@mexemai.com>
2026-03-10 12:25:03 +0500

    admin test

--- src/Pages/AdminLayout.jsx
+++ src/Pages/AdminLayout.jsx
@@ -5,6 +5,12 @@ const AdminLayout = () => {
     <div>
       {/* sidebar */}
       <p>AdminLayout </p>
+      <p>AdminLayout </p>
+      <p>AdminLayout </p>
+      <p>AdminLayout </p>
+      <p>AdminLayout </p>
+      <p>AdminLayout </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae quidem itaque consectetur eligendi eius, amet ex labore sequi vel explicabo, porro doloribus suscipit laborum repellat repellendus! Cumque cum harum iure? </p>
     </div>
   )
 }
