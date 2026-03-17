454e49825ccb8a8c740f1ee49bec7a93eb9a5090
zeshanzee22 <zeeshansaid@mexemai.com>
2026-03-17 11:09:25 +0500

    pricing setup

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,16 +1,21 @@
 import { create } from "zustand";
 
+ 
+
+
 export const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  token: localStorage.getItem("token") || null,
 
   login: (user, token) => {
     localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
     set({ user, token });
   },
 
   logout: () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     set({ user: null, token: null });
   },
 }));
